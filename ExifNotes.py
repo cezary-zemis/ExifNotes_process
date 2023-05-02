18ff9175d8000456433fe42f83d07778a824ed11
Cezary Żemis <cezary.zemis@gmail.com>
2023-05-02 14:30:25 +0200

    code cleanup; formatting exposure compensation as mixed numeral; adding spool description to header

--- ExifNotes.py
+++ ExifNotes.py
@@ -1,4 +1,4 @@
-import os, pathlib, sys, re, fnmatch
+import os, pathlib, sys, re, fnmatch, math, fractions
 from PIL.ExifTags import Base
 
 def inches(mm):
@@ -15,7 +15,10 @@ def read_exif(exif_data, key_name):
     if Base[key_name].value in exif_data:
         val = exif_data[Base[key_name].value]
     if type(val) is bytes :
-        val = val.decode()
+        try:
+            val = val.decode()
+        except UnicodeDecodeError:
+            pass
     return val
 
 def read_description(desc_str, key):
@@ -43,8 +46,52 @@ def get_image_files():
         image_files += [f for f in findfiles('*' + ext, path) if not re.search('contact[_-]sheet', os.path.basename(f))]
     image_files.sort()
     return image_files
+    
+def fraction_to_float(frac_str):
+    try:
+        return float(frac_str)
+    except ValueError:
+        num, denom = frac_str.split('/')
+        try:
+            leading, num = num.split(' ')
+            whole = float(leading)
+        except ValueError:
+            whole = 0
+        frac = float(num) / float(denom)
+        return whole - frac if whole < 0 else whole + frac
+
+def print_verbose(str) :
+    global verbose
+    if verbose :
+        print(str)
+        
+def nice_signed_fraction(fl) :
+    nsf = '+'
+    if fl < 0 :
+        nsf = '-'
+    i = math.floor(math.fabs(fl))
+    f = str(fractions.Fraction(math.fabs(fl) - i).limit_denominator(4))
+    if f == '1/4' :
+        f == '¼' 
+    elif f == '1/3' :
+        f = '⅓'
+    elif f == '1/2' :
+        f = '½'
+    elif f == '2/3' :
+        f = '⅔'
+    elif f == '3/4' :
+        f = '¾'
+    if i == 0 and f == '0' :
+        return '0'
+    else :
+        if i != 0 :
+            nsf += str(i)
+        if f != '0' :
+            nsf += f
+        return nsf
 
 dry_run = read_environ('DRY_RUN')
+verbose = read_environ('VERBOSE')
 path = read_environ('WORKING_DIR', '.')
 extensions = read_environ('EXTENSIONS', '.tif .tiff .jpg .jpeg').split(' ')
 exif_notes_extension = read_environ('EXIF_NOTES_EXTENSION', '.json')
